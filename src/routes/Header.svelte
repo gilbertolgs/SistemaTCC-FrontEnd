eda727008883af75a5d1643a133af9e655aee420
Gilberto Luís <gilbertoluisguedessilva@hotmail.com>
2023-08-26 16:42:54 -0300

    pagina de login

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -3,127 +3,62 @@
 	import logo from '$lib/images/svelte-logo.svg';
 	import github from '$lib/images/github.svg';
 </script>
-
-<header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
+<header class="bg-white">
+	<nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+	  <div class="flex lg:flex-1">
+		<a href="/" class="-m-1.5 p-1.5">
+		  <span class="sr-only">Your Company</span>
+		  <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
 		</a>
-	</div>
-
-	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
-			</li>
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
+	  </div>
+	  <div class="flex lg:hidden">
+		<button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+		  <span class="sr-only">Open main menu</span>
+		  <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+			<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+		  </svg>
+		</button>
+	  </div>
+	  <div class="hidden lg:flex lg:gap-x-12">
+  
+		<a href="/" class="text-sm font-semibold leading-6 text-gray-900">Features</a>
+		<a href="/" class="text-sm font-semibold leading-6 text-gray-900">Marketplace</a>
+		<a href="/" class="text-sm font-semibold leading-6 text-gray-900">Company</a>
+	  </div>
+	  <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+		<a href="/login" class="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+	  </div>
 	</nav>
-
-	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-			<img src={github} alt="GitHub" />
-		</a>
+	<!-- Mobile menu, show/hide based on menu open state. -->
+	<div class="lg:hidden" role="dialog" aria-modal="true">
+	  <!-- Background backdrop, show/hide based on slide-over state. -->
+	  <div class="fixed inset-0 z-10"></div>
+	  <div class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+		<div class="flex items-center justify-between">
+		  <a href="/" class="-m-1.5 p-1.5">
+			<span class="sr-only">Your Company</span>
+			<img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
+		  </a>
+		  <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+			<span class="sr-only">Close menu</span>
+			<svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+			  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+			</svg>
+		  </button>
+		</div>
+		<div class="mt-6 flow-root">
+		  <div class="-my-6 divide-y divide-gray-500/10">
+			<div class="space-y-2 py-6">
+			  <a href="/" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Features</a>
+			  <a href="/" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Marketplace</a>
+			  <a href="/" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Company</a>
+			</div>
+			<div class="py-6">
+			  <a href="/login" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
+			</div>
+		  </div>
+		</div>
+	  </div>
 	</div>
-</header>
-
-<style>
-	header {
-		display: flex;
-		justify-content: space-between;
-	}
-
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-
-	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
-	}
-
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
-	}
-
-	li {
-		position: relative;
-		height: 100%;
-	}
-
-	li[aria-current='page']::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 0.5rem;
-		color: var(--color-text);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
-
-	a:hover {
-		color: var(--color-theme-1);
-	}
-</style>
+  </header>
+  
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,37 @@
+<!-- component -->
+<div class="flex items-center">
+    <div class="h-max mx-auto flex flex-col items-center">
+        <img class="h-[40px] w-[47px] mb-5" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
+        <h1 class="text-xl font-bold text-center pb-10">Acesse com seu e-mail instrucional.</h1>
+        <div class="bg-white shadow-xl p-10 flex flex-col gap-4 text-sm">
+            <div>
+                <label class="text-gray-600 font-bold inline-block pb-2" for="email">Email</label>
+                <input class="border border-gray-400 focus:outline-slate-400 rounded-md w-full shadow-sm px-5 py-2" type="email" name="email" placeholder="exemplo@aedb.br">
+            </div>
+            <div>
+                <label class="text-gray-600 font-bold inline-block pb-2" for="password">Senha</label>
+                <input class="border border-gray-400 focus:outline-slate-400 rounded-md w-full shadow-sm px-5 py-2" type="password" name="password" placeholder="******">
+            </div>
+            <div class="flex">
+                <div class="w-1/2">
+                    <input type="checkbox" id="remeberMe">
+                    <label for="remeberMe">Lembrar E-mail</label>
+                </div>
+                <div class="w-1/2">
+                    <a class="font-bold text-blue-600" href="/">Esqueceu a senha?</a>
+                </div>
+            </div>
+            <div>
+                <input class="bg-[#4F46E5] w-full py-2 rounded-md text-white font-bold cursor-pointer hover:bg-[#181196]" type="submit" value="Login" >
+            </div>
+            <div>
+                <p class="text-center">Faça o login usando sua conta.</p>
+            </div>
+            <div class="flex place-content-center">
+                <button class="cursor-pointer hover:bg-[#eeeeee] rounded-xl">
+                    <img src="https://www.google.com/favicon.ico" alt="Google Logo" class="">
+                </button>
+            </div>
+        </div>        
+    </div>
+</div>
